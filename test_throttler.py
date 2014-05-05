e6fbd5f84bde997ef102531956bcca971b3a7047
Morgan Cromell <morgan.cromell@elev.it-gymnasiet.se>
2014-05-05 10:33:23 +0200

    Cleaning

--- test_throttler.py
+++
@@ -1,7 +0,0 @@
-from nose.tools import assert_equals
-from RedBot.throttler import Throttler
-
-
-def test_throttler_request_allowed():
-    t = Throttler(10, 10)
-    assert_equals(True, t.is_request_allowed)
