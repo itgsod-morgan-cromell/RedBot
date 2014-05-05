74e7d85f0abfb1f1eee3e4137575e4f264365d18
Viktor Lund <viktor.lund@elev.it-gymnasiet.se>
2014-05-05 10:27:36 +0200

    Commented on throttler and redbot

--- RedBot.py
+++ RedBot.py
@@ -1,18 +1,28 @@
 import requests
 import time
+
+
 class RedBot(object):
+    """
+    This class takes care of the request objects. If the throttler allows this class to send the request object
+    the class will process the request object.
+    This class also takes care of the client and keeps everything running.
+    The client is the session that makes us able to continue with the same login account.
+    """
+
     def __init__(self, throttler):
         self.client = requests.session()
-        self.client.headers = {'user-agent': 'u/RedBotITG\'s testbot, will post in all unposted posts. v 0.000000001'}
+        self.client.headers = {'user-agent': 'u/RedBotITG\'s testbot v1. Made for commenting on posts.'}
         self.throttler = throttler
         self.request_queue = []
         self.completed_requests = []
         self.process_queue()
 
 
+    #clear_completed_queue and process_queue are not currently used
+
     def clear_completed_queue(self):
         self.completed_requests = []
-
 
     def process_queue(self):
         for i in range(len(self.request_queue)):
--- throttler.py
+++ throttler.py
@@ -2,6 +2,11 @@ import time
 
 
 class Throttler(object):
+    """
+    The throttler keeps track of the amount of requests sent per minute.
+    If the amount of requests reaches 30 within a minute, the throttler prevents the RedBot to send more requests.
+    """
+
     def __init__(self, max_amount_of_requests_per_interval, time_interval_in_seconds):
         self.last_reset_in_time = int(time.time())
         self.processed_requests = 0
@@ -10,6 +15,10 @@ class Throttler(object):
 
     @property
     def is_request_allowed(self):
+        """
+        Checks if RedBot is allowed to send a request.
+        Returns a boolean.
+        """
         self._update()
         if self.processed_requests < self.max_amount_of_requests_per_interval:
             return True
@@ -21,6 +30,9 @@ class Throttler(object):
             self.processed_requests += 1
 
     def _update(self):
+        """
+        This function updates the timer and resets the timer to 0 when it reaches 60 seconds.
+        """
         self.passed_time_since_last_reset = int(time.time() - self.last_reset_in_time)
         if self.passed_time_since_last_reset >= self.reset_time_interval:
             self._reset()
